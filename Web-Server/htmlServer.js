4d1209571bd46bd22dfec903834471832a535c47
Aisha Gench <aisha.gench@gmail.com>
2021-05-22 00:46:07 -0500

    create index file

--- Web-Server/htmlServer.js
+++ Web-Server/htmlServer.js
@@ -3,20 +3,26 @@ const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
+let indexFile;
 
 const requestListener = function (req, res) {
-    fs.readFile(__dirname + "/index.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-        .catch(err => {
-            res.writeHead(500);
-            res.end(err);
-            return;
-        });
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile); 
   };
+
+  fs.readFile(__dirname + "/index.html")
+  .then(contents => {
+      indexFile = contents;
+      server.listen(port, host, () => {
+          console.log(`Server is running on http://${host}:${port}`);
+      });
+  })
+  .catch(err => {
+      console.error(`Could not read index.html file: ${err}`);
+      process.exit(1);
+  });
+
 
 
 const server = http.createServer(requestListener); 
