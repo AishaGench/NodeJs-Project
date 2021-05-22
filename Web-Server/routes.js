488ed7ad26ffe55c4f9a2a6738a2012d1f321a9f
Aisha Gench <aisha.gench@gmail.com>
2021-05-22 01:12:23 -0500

    add switch case

--- Web-Server/routes.js
+++ Web-Server/routes.js
@@ -3,12 +3,37 @@ const http = require('http')
 const host = 'localhost';
 const port = 8000;
 
+const books = JSON.stringify([
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+]);
+const authors = JSON.stringify([
+    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
+    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
+
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(`{"message": "This is a JSON response"}`);
-    
-};
+    switch (req.url) {
+        case "/":
+            res.writeHead(200);
+            res.end(`{"message": "This is a JSON response"}`);
+            break
+        case "/books":
+            res.writeHead(200);
+            res.end(books);
+            break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
+            
+    }
+}
+
 
 const server = http.createServer(requestListener); 
 
