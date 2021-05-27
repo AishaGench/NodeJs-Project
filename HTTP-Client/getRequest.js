78cbb4cb91f74a24fb5fe574507adaf2d7b58359
Aisha Gench <aisha.gench@gmail.com>
2021-05-26 21:09:49 -0500

    create get request second way

--- HTTP-Client/getRequest.js
+++ HTTP-Client/getRequest.js
@@ -31,4 +31,45 @@ request.on('error', (err) => {
     console.error(`Encountered an error trying to make a request: ${err.message}`);
   });
 */
+// --------- Get Requests - Second Way --------------
+
+
+const https = require("https");
+
+const options = {
+  method: 'GET',
+  host: 'jsonplaceholder.typicode.com',
+  path: '/users?_limit=2',
+  headers: {
+    'Accept': 'application/json'
+  }
+};
+
+let request = https.request(options,  (res) => {
+    //console.log(res)
+    if (res.statusCode !== 200) {
+        console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+        res.resume();
+        return;
+    };
+      let data = '';
+
+      res.on("data", (chunk) => {
+        data += chunk;
+        // console.log(chunk.toString())
+      });
+      
+      res.on('close', () => {
+        console.log('Retrieved all data');
+        console.log(JSON.parse(data));
+      });
+    
+ });
+
+ request.end()
+
+ request.on('error', (err) => {
+    console.error(`Encountered an error trying to make a request: ${err.message}`);
+  });
+  
   
