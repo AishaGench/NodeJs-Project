127845fd85f0799543a775c5e9ba52074fb4bc45
Aisha Gench <aisha.gench@gmail.com>
2021-05-28 20:42:55 -0500

    create routes

--- Express-Server2/node-express-CRUD/index.js
+++ Express-Server2/node-express-CRUD/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
-
+app.use(express.json());
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(3000, () => console.log(`Server is running on port!`))
