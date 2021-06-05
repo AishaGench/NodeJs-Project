256cf97313716eb217e78f9dbd8c64c8f80bc8f5
Aisha Gench <aisha.gench@gmail.com>
2021-06-04 21:05:41 -0500

    create post request

--- NodeExpressMongoStarterPack/server.js
+++ NodeExpressMongoStarterPack/server.js
@@ -1,11 +1,29 @@
 const express = require('express')
+var bodyParser = require('body-parser');
+
 const app =express();
 const port = 3000;
 
-app.get('/', (req,res)=>{
-    res.setEncoding("Hello world!")
+app.get("/", (req,res)=>{
+    // res.send("Hello world!");
+    res.sendFile(__dirname + "/index.html")
+
 }) 
 
-app.listen(port,(req,res=>{
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.post('/update', function(req, res) {
+  const { name, description } = req.body;
+  res.send(`Name name, desc description`);
+});
+
+
+app.post("/quotes",((req,res)=>{
+    console.log("This is Post request")
+}))
+
+
+
+app.listen(port,((req,res)=>{
     console.log(`The server is working on ${port} port`)
 }))
