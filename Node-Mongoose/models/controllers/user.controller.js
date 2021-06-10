15d9b29b5b43248fa5b7d66b0a336eb6565a58fb
Aisha Gench <aisha.gench@gmail.com>
2021-06-09 21:48:12 -0500

    create function and exports

--- Node-Mongoose/models/controllers/user.controller.js
+++ Node-Mongoose/models/controllers/user.controller.js
@@ -1,1 +1,18 @@
 const User = require("../models/user.model")
+
+exports.create = (req,res) => {
+    const user = new User({
+        email:req.body.email,
+        name:req.body.name,
+        password:req.body.password,
+        age:req.body.age,
+        gender:req.body.gender,
+        isActive:req.body.isActive,
+        userType:req.body.userType,
+    })
+​
+    user.save()
+    .then((data)=>{res.send(data)})
+    .catch((err)=>{res.send(err)})
+​
+}
